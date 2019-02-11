937bc56cb46c3fb88f365fa184d087e2021379dd
Putra <putraxor@gmail.com>
2019-02-12 03:09:36 +0700

    return system temp directory

--- lib/src/utils/path_provider.dart
+++ lib/src/utils/path_provider.dart
@@ -1,9 +1,10 @@
 part of flutter_parse_sdk;
 
 Future<Directory> getTemporaryDirectory() async {
-  Directory path = Directory('${Directory.current.path}/tmp');
-  if (!path.existsSync()) {
-    path.createSync(recursive: true);
-  }
-  return path;
+  return Directory.systemTemp;
+  // Directory path = Directory('${Directory.current.path}/tmp');
+  // if (!path.existsSync()) {
+  //   path.createSync(recursive: true);
+  // }
+  // return path;
 }
